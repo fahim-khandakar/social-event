1fc9bd8fa889ea56ddd7d70029f2cb7cf05e8ae3
fahim-khandakar <fahimkhandakar01@gmail.com>
2023-10-07 16:05:29 +0600

    changed banner section image and title

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -1,12 +1,28 @@
 import banner from "../../assets/banner3.jpg";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Banner = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
-    <div className="w-full object-cover">
-      <div>
-        <img className="w-full max-h-screen" src={banner} alt="" />
-        <div className="absolute top-80 text-center right-0 left-0 bg-[#00000070]  ">
-          <h1 className="text-5xl  font-bold text-white  py-5">
+    <div className="max-w-6xl mx-auto p-5 md:p-0 mt-24">
+      <div className="flex flex-col md:flex-row-reverse justify-around items-center gap-10">
+        <div
+          className="w-full md:w-1/2"
+          data-aos="fade-down-left"
+          data-aos-duration="2000"
+        >
+          <img className="w-full rounded-lg" src={banner} alt="" />
+        </div>
+        <div
+          className="w-full md:w-1/2"
+          data-aos="fade-down-right"
+          data-aos-duration="2000"
+        >
+          <h1 className="text-7xl  font-bold text-[#000000af]   py-5">
             We Are Ready For Your Events!
           </h1>
         </div>
