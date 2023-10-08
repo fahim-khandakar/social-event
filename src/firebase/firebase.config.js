18940aa10eab41b5945218aced33bb2917b90c2f
fahim-khandakar <fahimkhandakar01@gmail.com>
2023-10-09 01:58:43 +0600

    changed firebase file and added favicon

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBuMNbajI0dRtPOKXscue-_ZJkNOSoJoHo",
-  authDomain: "social-events-b301c.firebaseapp.com",
-  projectId: "social-events-b301c",
-  storageBucket: "social-events-b301c.appspot.com",
-  messagingSenderId: "28003241020",
-  appId: "1:28003241020:web:cafbfc44ecdda24f024739",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
